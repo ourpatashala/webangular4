e6f5a12845e7c8b08d1f16671e1d2c62c38e94cc
preneeth <preneetho@gmail.com>
2017-11-03 22:31:41 +0530

    Remove Photo functionality Implemented

--- patashala4/src/app/constants/AppConstants.ts
+++ patashala4/src/app/constants/AppConstants.ts
@@ -7,7 +7,7 @@ import {Injectable} from "@angular/core";
 export class AppConstants{
   public static TEST_CONSTANT='Test Constant';
   public static PhotoMaxSize =100000;
-  public static IMAGE_ERROR_MESSAGE ='Image To long, Please use image less than 100KB';
+  public static IMAGE_ERROR_MESSAGE ='Cannot upload images more than 100KB';
 
   public static SHAREDPREFERANCE_SCHOOLID ="SchoolID";
   public static SHAREDPREFERANCE_STUDENTID ="StudentID";
@@ -16,6 +16,6 @@ export class AppConstants{
   public static DEFAULT_STUDENT_IMAGE ="./assets/images/profile/default.jpg";
   public static DEFAULT_SCHOOL_LOGO ="./assets/images/profile/default.jpg";
   public static month_names_short= ['','Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  
-  
+
+
 }
