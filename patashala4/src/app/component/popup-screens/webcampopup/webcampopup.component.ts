618029556a4594e86d3cd505c9baff23dafd46aa
NithinNitz <nithin.logicp@gmail.com>
2017-11-24 06:30:33 +0530

    Camera Upload implemented

--- patashala4/src/app/component/popup-screens/webcampopup/webcampopup.component.ts
+++ patashala4/src/app/component/popup-screens/webcampopup/webcampopup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { WebCamComponent } from 'ack-angular-webcam';
+import {FileUpload} from '../../../service/fileupload';
+import { AppConstants } from "../../../constants/AppConstants";
+import {UploadFileService} from '../../../service/upload-file.service';
 
 @Component({
   selector: 'app-webcampopup',
@@ -10,8 +13,17 @@ export class WebcampopupComponent implements OnInit {
   webcam:WebCamComponent//will be populated by <ack-webcam [(ref)]="webcam">
   base64;
   config;
-
-  constructor() { }
+  selectedimagesrc: string;
+  currentFileUpload: FileUpload;
+  progress: {percentage: number} = {percentage: 0};
+  active: string = "0";
+  errorMessage: string;
+  sucessMessage: string;
+    popupstatus: string = "0";
+	  showupload: string = "0"; 
+	  
+	  
+  constructor( private uploadService: UploadFileService) { }
 
   ngOnInit() {
   }
@@ -20,31 +32,52 @@ export class WebcampopupComponent implements OnInit {
     this.webcam.getBase64()
     .then( base=>{
       this.base64=base;
-      alert(base);
+	  this.selectedimagesrc=base;
     })
     .catch( e=>console.error(e) )
   }
 
   //get HTML5 FormData object and pretend to post to server
   genPostData(){
-    this.webcam.captureAsFormData({fileName:'file.jpg'})
-    .then( formData=>this.postFormData(formData) )
-    .catch( e=>console.error(e) )
+  
+var blob = new Blob([this.selectedimagesrc], {type: 'image/png'});
+
+
+var file = new File([blob], 'imageFileName.png');
+
+    this.currentFileUpload = new FileUpload(file);
+      console.log(localStorage.getItem(AppConstants.SHAREDPREFERANCE_SCHOOLID)+"    "+localStorage.getItem(AppConstants.SHAREDPREFERANCE_STUDENTID)+"  "+this.currentFileUpload+"   "+file);
+      this.uploadService.pushFileToStorage(localStorage.getItem(AppConstants.SHAREDPREFERANCE_SCHOOLID),  localStorage.getItem(AppConstants.SHAREDPREFERANCE_STUDENTID), this.currentFileUpload, this.progress);
+      this.updateProgressbarUI();
   }
 
   //a pretend process that would post the webcam photo taken
   postFormData(formData){
-    this.config = {
-      method:"post",
-      url:"http://www.aviorsciences.com/",
-      body: formData
-    }
-
-    let request = new Request(this.config);
+   console.log("test data"+formData.fileName);
 
 }
+updateProgressbarUI()
+  {
+    console.log(" updateProgressbarUI  "+this.progress.percentage);
+    if(this.progress.percentage==100)
+    {
+      this.currentFileUpload=null;
+      this.active="1";
+      this.sucessMessage="Image Uploaded Successfully";
+      this.popupstatus = "0";
+      this.showupload= "0";
+    }
+    else
+    {
+      setTimeout(() => {   this.updateProgressbarUI(); }, 500);
+    }
+  }
 
-  onCamError(err){}
+  onCamError(err){
+    alert(err);
+  }
 
-  onCamSuccess(){}
+  onCamSuccess(event){
+    console.log(event);
+  }
 }
